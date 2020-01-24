ddb9a1e86fc4c6b3ba627d437b01f8fdae51fa4a
Roman Kyrychenko <roman.kyrychenko@kyivstar.net>
2020-01-24 19:09:41 +0200

    refresh radar googleVis chart

--- radar.R
+++ radar.R
@@ -1,12 +1,12 @@
-pk <- c("Інститути", "Інфраструктура", "Макроекономіка", "Здоров'я", 
-        "Вища освіта", "Ринки", "Праця",
-        "Фін. ринок", "Технології", "Розмір ринку", "Бізнес",
-        "Інновації")
-rs <- c(3,3.9,3.2,6,5.1,4,4.2,3,3.6,4.4,3.6,3.4)
-pok <- data.frame(pk,rs)
-
-Gauge <-  gvisGauge(pok, 
-                    options=list(min=0, max=7, greenFrom=4.66,
-                                 greenTo=7, yellowFrom=2.33, yellowTo=4.66,
-                                 redFrom=0, redTo=2.33, width=900, height=600))
-plot(Gauge)
+plot(googleVis::gvisGauge(data.frame(
+  pk = c(
+    "Інститути", "Інфраструктура", "Макроекономіка", "Здоров'я",
+    "Вища освіта", "Ринки", "Праця", "Фін. ринок", "Технології",
+    "Розмір ринку", "Бізнес", "Інновації"
+  ),
+  rs = c(3, 3.9, 3.2, 6, 5.1, 4, 4.2, 3, 3.6, 4.4, 3.6, 3.4)
+), options = list(
+  min = 0, max = 7, greenFrom = 4.66,
+  greenTo = 7, yellowFrom = 2.33, yellowTo = 4.66,
+  redFrom = 0, redTo = 2.33, width = 900, height = 600
+)))
